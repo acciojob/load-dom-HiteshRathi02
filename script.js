513725a2b3f808ffbe58197a5b152bb217d0a702
Hitesh Rathi <97667730+HiteshRathi02@users.noreply.github.com>
2025-07-10 08:11:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+window.addEventListener("DOMContentLoaded", function () {
+  const body = document.querySelector("body");
+  const para = document.createElement("p");
+  para.innerHTML = "DOM load success";
+  body.appendChild(para);
+	
+});
